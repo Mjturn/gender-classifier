b66da0a6ce1e6699a269246b1c9c469fc907de24
Mjturn <Mjturn@outlook.com>
2024-05-28 14:42:50 -0400

    create training_data array

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 import os
 import cv2
+from random import shuffle
 
 DATA_DIRECTORY = "dataset/Training"
 GENDERS = ["male", "female"]
 
-for gender in GENDERS:
+training_data = []
+
+for gender_index, gender in enumerate(GENDERS):
     path = os.path.join(DATA_DIRECTORY, gender)
 
     for image in os.listdir(path):
         image_array = cv2.imread(os.path.join(path, image), cv2.IMREAD_GRAYSCALE)
+        training_data.append([image_array, gender_index])
+
+shuffle(training_data)
+
+X = []
+y = []
+
+for features, label in training_data:
+    X.append(features)
+    y.append(label)
