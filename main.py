027ce8f726f3e53c2b619e75dfc1098e85678404
Mjturn <Mjturn@outlook.com>
2024-05-29 18:24:36 -0400

    create model

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import os
 import cv2
 from random import shuffle
+import numpy
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, Activation, MaxPooling2D, Flatten, Dense
 
 DATA_DIRECTORY = "dataset/Training"
 GENDERS = ["male", "female"]
 
+IMAGE_SIZE = 100
 training_data = []
 
 for gender_index, gender in enumerate(GENDERS):
@@ -12,6 +16,7 @@ for gender_index, gender in enumerate(GENDERS):
 
     for image in os.listdir(path):
         image_array = cv2.imread(os.path.join(path, image), cv2.IMREAD_GRAYSCALE)
+        image_array = cv2.resize(image_array, (IMAGE_SIZE, IMAGE_SIZE))
         training_data.append([image_array, gender_index])
 
 shuffle(training_data)
@@ -22,3 +27,24 @@ y = []
 for features, label in training_data:
     X.append(features)
     y.append(label)
+
+X = numpy.array(X).reshape(-1, IMAGE_SIZE, IMAGE_SIZE, 1)
+y = numpy.array(y)
+
+X = X / 255
+
+model = Sequential([
+    Conv2D(64, (3,3), input_shape=X.shape[1:], activation="relu"),
+    MaxPooling2D(pool_size=(2,2)),
+
+    Conv2D(64, (3,3), activation="relu"),
+    MaxPooling2D(pool_size=(2,2)),
+
+    Flatten(),
+    Dense(64),
+    
+    Dense(1, activation="sigmoid"),
+    ])
+
+model.compile(loss="binary_crossentropy", optimizer="adam", metrics=["accuracy"])
+model.fit(X, y, batch_size=32, epochs=5, validation_split=0.1)
